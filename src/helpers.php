3929e33aeacdbacd8ffb78453254e7e84eab115d
wilkques <wilkques@gmail.com>
2023-06-07 15:14:09 +0800

    init

---
+++ src/helpers.php
@@ -0,0 +1,240 @@
+<?php
+
+if (!function_exists('string_snake')) {
+    /**
+     * @param string $camelCase
+     * 
+     * @return array|string|null
+     */
+    function string_snake($camelCase)
+    {
+        return preg_replace_callback(
+            ["/([A-Z]+)/", "/_([A-Z]+)([A-Z][a-z])/"],
+            function ($matches) {
+                return "_" . lcfirst($matches[0]);
+            },
+            $camelCase
+        );
+    }
+}
+
+if (!function_exists('array_key_sanke')) {
+    /**
+     * String snake to study
+     * 
+     * @param array $array
+     * 
+     * @return array
+     */
+    function array_key_sanke($array)
+    {
+        return array_combine(string_snake(array_keys($array)), $array);
+    }
+}
+
+if (!function_exists('data_set')) {
+    /**
+     * Set an item on an array or object using dot notation.
+     *
+     * @param  mixed  $target
+     * @param  string|array  $key
+     * @param  mixed  $value
+     * @param  bool  $overwrite
+     * @return mixed
+     */
+    function data_set(&$target, $key, $value, $overwrite = true)
+    {
+        $segments = is_array($key) ? $key : explode('.', $key);
+
+        if (($segment = array_shift($segments)) === '*') {
+            if (!accessible($target)) {
+                $target = [];
+            }
+
+            if ($segments) {
+                foreach ($target as &$inner) {
+                    data_set($inner, $segments, $value, $overwrite);
+                }
+            } elseif ($overwrite) {
+                foreach ($target as &$inner) {
+                    $inner = $value;
+                }
+            }
+        } elseif (accessible($target)) {
+            if ($segments) {
+                if (!exists($target, $segment)) {
+                    $target[$segment] = [];
+                }
+
+                data_set($target[$segment], $segments, $value, $overwrite);
+            } elseif ($overwrite || !exists($target, $segment)) {
+                $target[$segment] = $value;
+            }
+        } elseif (is_object($target)) {
+            if ($segments) {
+                if (!isset($target->{$segment})) {
+                    $target->{$segment} = [];
+                }
+
+                data_set($target->{$segment}, $segments, $value, $overwrite);
+            } elseif ($overwrite || !isset($target->{$segment})) {
+                $target->{$segment} = $value;
+            }
+        } else {
+            $target = [];
+
+            if ($segments) {
+                data_set($target[$segment], $segments, $value, $overwrite);
+            } elseif ($overwrite) {
+                $target[$segment] = $value;
+            }
+        }
+
+        return $target;
+    }
+}
+
+if (!function_exists('accessible')) {
+    /**
+     * Determine whether the given value is array accessible.
+     *
+     * @param  mixed  $value
+     * @return bool
+     */
+    function accessible($value)
+    {
+        return is_array($value) || $value instanceof \ArrayAccess;
+    }
+}
+
+if (!function_exists('accessible')) {
+    /**
+     * Determine if the given key exists in the provided array.
+     *
+     * @param  \ArrayAccess|array  $array
+     * @param  string|int  $key
+     * @return bool
+     */
+    function exists($array, $key)
+    {
+        if ($array instanceof \ArrayAccess) {
+            return $array->offsetExists($key);
+        }
+
+        return array_key_exists($key, $array);
+    }
+}
+
+if (!function_exists('data_get')) {
+    /**
+     * Get an item from an array or object using "dot" notation.
+     *
+     * @param  mixed  $target
+     * @param  string|array|int|null  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    function data_get($target, $key, $default = null)
+    {
+        if (is_null($key)) {
+            return $target;
+        }
+
+        $key = is_array($key) ? $key : explode('.', $key);
+
+        foreach ($key as $i => $segment) {
+            unset($key[$i]);
+
+            if (is_null($segment)) {
+                return $target;
+            }
+
+            if ($segment === '*') {
+                if (!is_iterable($target)) {
+                    return value($default);
+                }
+
+                $result = [];
+
+                foreach ($target as $item) {
+                    $result[] = data_get($item, $key);
+                }
+
+                return $result;
+            }
+
+            if (is_array($target) && array_key_exists($segment, $target)) {
+                $target = $target[$segment];
+            } elseif (is_object($target) && isset($target->{$segment})) {
+                $target = $target->{$segment};
+            } else {
+                return value($default);
+            }
+        }
+
+        return $target;
+    }
+}
+
+if (!file_exists('is_iterable')) {
+    function is_iterable($obj)
+    {
+        return is_array($obj) || $obj instanceof \Traversable;
+    }
+}
+
+if (!function_exists('value')) {
+    /**
+     * Return the default value of the given value.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    function value($value, $args)
+    {
+        return $value instanceof \Closure ? call_user_func_array($value, $args) : $value;
+    }
+}
+
+if (!function_exists('array_take_off_recursive')) {
+    /**
+     * @param array &$array
+     * @param string $key
+     * @param mixed $default
+     * 
+     * @return mixed
+     */
+    function array_take_off_recursive(&$array, $key, $default = null)
+    {
+        $keys = explode('.', $key);
+        $currentKey = array_shift($keys);
+
+        if ($currentKey === '*') {
+            $values = array();
+
+            foreach ($array as $subKey => &$subArray) {
+                if (empty($keys)) {
+                    $values[] = $subArray;
+                    unset($array[$subKey]);
+                } else {
+                    $values[$subKey] = array_take_off_recursive($subArray, implode('.', $keys));
+                }
+            }
+
+            return $values;
+        }
+
+        if (array_key_exists($currentKey, $array)) {
+            if (empty($keys)) {
+                $target = $array[$currentKey];
+                unset($array[$currentKey]);
+
+                return $target;
+            } else {
+                return array_take_off_recursive($array[$currentKey], implode('.', $keys));
+            }
+        }
+
+        return $default;
+    }
+}
