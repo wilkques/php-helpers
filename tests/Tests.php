37a4470925846ccd623601a4be98c3e829529190
wilkques <wilkques@gmail.com>
2024-05-31 18:09:34 +0800

    fix php 5.6

---
+++ tests/Tests.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Wilkques\Helpers\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ArraysTest extends TestCase
+{
+    /**
+     * Asserts that a variable is of type int.
+     *
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws ExpectationFailedException
+     *
+     * @psalm-assert int $actual
+     */
+    public static function assertIsInt($actual, string $message = ''): void
+    {
+        if (PHP_MAJOR_VERSION > 5) {
+            return parent::assertIsInt($actual, $message);
+        }
+
+        assert(is_int($actual), $message);
+    }
+
+    /**
+     * Asserts that a variable is of type int.
+     *
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws ExpectationFailedException
+     *
+     * @psalm-assert int $actual
+     */
+    public static function assertIsString($actual, string $message = ''): void
+    {
+        if (PHP_MAJOR_VERSION > 5) {
+            return parent::assertIsString($actual, $message);
+        }
+
+        assert(is_int($actual), $message);
+    }
+
+    /**
+     * Asserts that a variable is of type int.
+     *
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws ExpectationFailedException
+     *
+     * @psalm-assert int $actual
+     */
+    public static function assertIsArray($actual, string $message = ''): void
+    {
+        if (PHP_MAJOR_VERSION > 5) {
+            return parent::assertIsArray($actual, $message);
+        }
+
+        assert(is_int($actual), $message);
+    }
+}
