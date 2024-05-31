2fadf899c7aaba2b185b65240f0adbec71c1e3f8
wilkques@gmail.com <wilkques@gmail.com>
2024-05-31 23:28:29 +0800

    fix test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,64 @@
 
 namespace Wilkques\Helpers\Tests;
 
-use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Tests\TestCase as BaseTestCase;
 
-class TestCase extends OrchestraTestCase
+class TestCase extends BaseTestCase
 {
+    /**
+     * Asserts that a variable is of type int.
+     *
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws ExpectationFailedException
+     *
+     * @psalm-assert int $actual
+     */
+    public static function assertIsInt($actual, string $message = ''): void
+    {
+        if (PHP_MAJOR_VERSION >= 7) {
+            parent::assertIsInt($actual, $message);
+
+            return;
+        }
+
+        parent::assertTrue(is_int($actual), $message);
+    }
+
+    /**
+     * Asserts that a variable is of type string.
+     *
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws ExpectationFailedException
+     *
+     * @psalm-assert string $actual
+     */
+    public static function assertIsString($actual, string $message = ''): void
+    {
+        if (PHP_MAJOR_VERSION >= 7) {
+            parent::assertIsString($actual, $message);
+
+            return;
+        }
+
+        parent::assertTrue(is_string($actual), $message);
+    }
+    
+    /**
+     * Asserts that a variable is of type array.
+     *
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws ExpectationFailedException
+     *
+     * @psalm-assert array $actual
+     */
+    public static function assertIsArray($actual, string $message = ''): void
+    {
+        if (PHP_MAJOR_VERSION >= 7) {
+            parent::assertIsArray($actual, $message);
+
+            return;
+        }
+
+        parent::assertTrue(is_array($actual), $message);
+    }
 }
