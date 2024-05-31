08784dc1a7f75039d611adaa7d443a75cbb90b36
wilkques@gmail.com <wilkques@gmail.com>
2024-05-31 22:56:35 +0800

    fix tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,54 +6,5 @@ use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
 {
-    /**
-     * Asserts that a variable is of type int.
-     *
-     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
-     * @throws ExpectationFailedException
-     *
-     * @psalm-assert int $actual
-     */
-    public static function assertIsInt($actual, string $message = ''): void
-    {
-        if (PHP_MAJOR_VERSION > 5) {
-            return parent::assertIsInt($actual, $message);
-        }
-
-        assert(is_int($actual), $message);
-    }
-
-    /**
-     * Asserts that a variable is of type int.
-     *
-     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
-     * @throws ExpectationFailedException
-     *
-     * @psalm-assert int $actual
-     */
-    public static function assertIsString($actual, string $message = ''): void
-    {
-        if (PHP_MAJOR_VERSION > 5) {
-            return parent::assertIsString($actual, $message);
-        }
-
-        assert(is_int($actual), $message);
-    }
-
-    /**
-     * Asserts that a variable is of type int.
-     *
-     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
-     * @throws ExpectationFailedException
-     *
-     * @psalm-assert int $actual
-     */
-    public static function assertIsArray($actual, string $message = ''): void
-    {
-        if (PHP_MAJOR_VERSION > 5) {
-            return parent::assertIsArray($actual, $message);
-        }
-
-        assert(is_int($actual), $message);
-    }
+    
 }
